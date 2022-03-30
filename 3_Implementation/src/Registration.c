725b6438f34998efa21e03f3af51eaffe857709e
Nandinikona02 <102673366+Nandinikona02@users.noreply.github.com>
2022-03-30 19:15:38 +0530

    Create Registration.c

---
+++ 3_Implementation/src/Registration.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "regist.h"
+
+int regist_status(long p){
+
+    // Checks for registration details of new patients    
+    pd[0].aadhar_number=p;
+    int i=1;
+    while(i<=1000){
+        if(pd[i].aadhar_number==pd[0].aadhar_number{
+            if(pd[i].vaccine_type==0)
+                return 0;
+            else
+                return 1;
+        }
+        i++;
+    }
+    return -1;
+}
+
+// Checks how many doses should be given
+int vaccine_status(int i){
+     
+    if (pd[i].no.ofdoses==0){
+        pd[i].no.ofdoses++;
+        return 1;}
+    else if(pd[i].no.ofdoses==1){
+        pd[i].no.ofdoses++;
+        return 2;}
+    else if (pd[i].no.ofdoses==2){
+        return 3;}
+    else
+        return 0;
+
+}
