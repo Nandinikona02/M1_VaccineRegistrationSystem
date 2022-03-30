61202753ceed8bdab7880cc69ab8b39a4c23fca4
Nandinikona02 <102673366+Nandinikona02@users.noreply.github.com>
2022-03-30 19:56:19 +0530

    Update Registration.c

--- 3_Implementation/src/Registration.c
+++ 3_Implementation/src/Registration.c
@@ -18,18 +18,3 @@ int regist_status(long p){
     return -1;
 }
 
-// Checks how many doses should be given
-int vaccine_status(int i){
-     
-    if (pd[i].no.ofdoses==0){
-        pd[i].no.ofdoses++;
-        return 1;}
-    else if(pd[i].no.ofdoses==1){
-        pd[i].no.ofdoses++;
-        return 2;}
-    else if (pd[i].no.ofdoses==2){
-        return 3;}
-    else
-        return 0;
-
-}
